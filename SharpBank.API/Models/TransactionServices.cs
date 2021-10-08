190194221fab98498640925f3fdb1c7f71731603
Shriram <18211a04l2@bvrit.ac.in>
2021-10-08 21:10:42 +0530

    Added Services on API

---
+++ SharpBank.API/Models/TransactionServices.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharpBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpBank.API.Models
+{
+    public class TransactionServices : ITransactionServices
+    {
+        private readonly AppDbContext appDbContext;
+
+        public TransactionServices(AppDbContext appDbContext) {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<Transaction> AddTransaction(Transaction transaction)
+        {
+            var result = await appDbContext.Transactions.AddAsync(transaction);
+            await appDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<Transaction> GetTransaction(string TransactionID)
+        {
+            return await appDbContext.Transactions.FirstOrDefaultAsync(e=>e.TransactionID==TransactionID);
+        }
+
+        public async Task<IEnumerable<Transaction>> GetTransactions()
+        {
+            return await appDbContext.Transactions.ToListAsync();
+        }
+    }
+}
