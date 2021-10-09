7abca7482eb1fd1f43a23cea672de9c266571a03
Shriram <18211a04l2@bvrit.ac.in>
2021-10-09 18:44:41 +0530

    Added Transaction Services

---
+++ SharpBank.Web/Pages/TransactionsListBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using SharpBank.Models;
+using SharpBank.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpBank.Web.Pages
+{
+
+    public class TransactionsListBase : ComponentBase
+    {
+        [Inject]
+        public ITransactionServices TransactionServices { get; set; }
+
+        public IEnumerable<Transaction> Transactions { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Transactions = (await TransactionServices.GetTransactions()).ToList();
+        }
+    }
+}
