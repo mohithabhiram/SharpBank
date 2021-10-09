77eaa56fd69a9e0622f6efabea1a0eb4c0b345e4
Shriram <18211a04l2@bvrit.ac.in>
2021-10-09 09:24:42 +0530

    Added Database Injection For Accounts Table

---
+++ SharpBank.Web/Services/AccountServices.cs
@@ -0,0 +1,25 @@
+﻿using SharpBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace SharpBank.Web.Services
+{
+    public class AccountServices : IAccountServices
+    {
+        private readonly HttpClient httpClient;
+
+        public AccountServices(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Account>> GetAccounts()
+        {
+            return await httpClient.GetFromJsonAsync<Account[]>("api/accounts");
+        }
+    }
+}
