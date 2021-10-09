435c0c268a4dcd407b31ab1ad121692bce383673
Shriram <18211a04l2@bvrit.ac.in>
2021-10-09 18:44:41 +0530

    Added Transaction Services

---
+++ SharpBank.Web/Services/BankServices.cs
@@ -0,0 +1,25 @@
+﻿using SharpBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace SharpBank.Web.Services
+{
+    public class BankServices : IBankServices
+    {
+        private readonly HttpClient httpClient;
+
+        public BankServices(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Bank>> GetBanks()
+        {
+            return await httpClient.GetFromJsonAsync<Bank[]>("api/banks");
+        }
+    }
+}
