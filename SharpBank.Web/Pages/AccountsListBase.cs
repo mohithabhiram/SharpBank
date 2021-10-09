f317543b1756a5b1a27ac5d46c211e97b3b40761
Shriram <18211a04l2@bvrit.ac.in>
2021-10-09 09:24:42 +0530

    Added Database Injection For Accounts Table

---
+++ SharpBank.Web/Pages/AccountsListBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using SharpBank.Models;
+using SharpBank.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpBank.Web.Pages
+{
+    public class AccountsListBase:ComponentBase
+    {
+        [Inject]
+        public IAccountServices AccountServices { get; set; }
+
+        public IEnumerable<Account> Accounts { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Accounts = (await AccountServices.GetAccounts()).ToList();
+        }
+    }
+}
